41e19405f53ca2f105d84b898c22d38c2eb90ac5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-10-01 10:15:45 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,9 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
-// Class to define methods for commonly used Redis commands
+/**
+ * RedisClient class for interacting with a Redis server.
+ */
 class RedisClient {
   constructor() {
     this.client = createClient();
@@ -10,19 +12,36 @@ class RedisClient {
     });
   }
 
-  // Check connection status and report
+  /**
+   * Checks if the Redis client is connected to the server.
+   * @returns {boolean} True if connected, false otherwise.
+   */
   isAlive() {
     return this.client.connected;
   }
 
-  // Get value for a given key from the Redis server
+  /**
+   * Retrieves the value associated with the given key
+   * from the Redis server.
+   * @param {string} key - The key to retrieve the value for.
+   * @returns {Promise<string|null>} The value associated
+   * with the key, or null if the key doesn't exist.
+   */
   async get(key) {
     const redisGet = promisify(this.client.get).bind(this.client);
     const value = await redisGet(key);
     return value;
   }
 
-  // Set a key-value pair in the Redis server with an optional expiration time
+  /**
+   * Stores a key-value pair in the Redis server with an
+   * optional expiration time.
+   * @param {string} key - The key to associate the value with.
+   * @param {string} value - The value to store.
+   * @param {number} [expireSeconds] - The number of seconds after
+   * which the key-value pair should expire. If not provided,
+   * the pair won't expire.
+   */
   async set(key, value, expireSeconds) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
@@ -32,7 +51,10 @@ class RedisClient {
     }
   }
 
-  // Remove a key-value pair from the Redis server
+  /**
+   * Removes a key-value pair from the Redis server.
+   * @param {string} key - The key to remove.
+   */
   async del(key) {
     const redisDel = promisify(this.client.del).bind(this.client);
     await redisDel(key);
