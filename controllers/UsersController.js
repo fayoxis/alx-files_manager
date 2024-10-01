a3db393ca282e6e85609ec6aca7724b0b32f44a9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-10-01 15:58:34 +0100

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,10 @@
-const sha1 = require('sha1');
-const { ObjectId } = require('mongodb');
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
-  static async postNew(request, response) {
+  static async postNew (request, response) {
     const { email, password } = request.body;
     if (!email) {
       response.status(400).json({ error: 'Missing email' });
@@ -22,10 +22,9 @@ class UsersController {
       if (user1) {
         response.status(400).json({ error: 'Already exist' });
       } else {
-        await collection.insertOne({ email, password: hashPwd });
+        collection.insertOne({ email, password: hashPwd });
         const newUser = await collection.findOne(
-          { email },
-          { projection: { email: 1 } }
+          { email }, { projection: { email: 1 } }
         );
         response.status(201).json({ id: newUser._id, email: newUser.email });
       }
@@ -35,16 +34,18 @@ class UsersController {
     }
   }
 
-  static async getMe(request, response) {
+  static async getMe (request, response) {
     try {
       const userToken = request.header('X-Token');
       const authKey = `auth_${userToken}`;
+      // console.log('USER TOKEN GET ME', userToken);
       const userID = await redisClient.get(authKey);
       console.log('USER KEY GET ME', userID);
       if (!userID) {
         response.status(401).json({ error: 'Unauthorized' });
       }
       const user = await dbClient.getUser({ _id: ObjectId(userID) });
+      // console.log('USER GET ME', user);
       response.json({ id: user._id, email: user.email });
     } catch (error) {
       console.log(error);
@@ -53,4 +54,5 @@ class UsersController {
   }
 }
 
-module.exports = UsersController;
+export default UsersController;
+// export default postNew();
