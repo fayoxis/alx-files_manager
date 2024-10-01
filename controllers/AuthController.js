2842087becce0498cf2142ec279dbc77a0b0dd4a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-10-01 17:27:19 +0100

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,40 +5,43 @@ import redisClient from '../utils/redis';
 
 class AuthController {
   static async getConnect(request, response) {
-    let userEmail;
-    let data;
-    do {
-      const authData = request.header('Authorization');
-      userEmail = authData.split(' ')[1];
-      const buff = Buffer.from(userEmail, 'base64');
-      userEmail = buff.toString('ascii');
-      data = userEmail.split(':'); // contains email and password
-    } while (data.length !== 2);
+    const authData = request.header('Authorization');
+    let userEmail = authData.split(' ')[1];
+    let buff = Buffer.from(userEmail, 'base64');
+    userEmail = buff.toString('ascii');
+    const data = userEmail.split(':');
+    if (data.length !== 2) {
+      response.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
 
     const hashedPassword = sha1(data[1]);
     const users = dbClient.db.collection('users');
-    users.findOne({ email: data[0], password: hashedPassword }, async (err, user) => {
+    let user;
+    let cursor = users.find({ email: data[0], password: hashedPassword });
+    while (await cursor.hasNext()) {
+      user = await cursor.next();
       if (user) {
         const token = uuidv4();
         const key = `auth_${token}`;
         await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
         response.status(200).json({ token });
-      } else {
-        response.status(401).json({ error: 'Unauthorized' });
+        return;
       }
-    });
+    }
+    response.status(401).json({ error: 'Unauthorized' });
   }
 
   static async getDisconnect(request, response) {
     const token = request.header('X-Token');
     const key = `auth_${token}`;
-    const id = await redisClient.get(key);
-    if (id) {
+    let id = await redisClient.get(key);
+    while (id) {
       await redisClient.del(key);
       response.status(204).json({});
-    } else {
-      response.status(401).json({ error: 'Unauthorized' });
+      return;
     }
+    response.status(401).json({ error: 'Unauthorized' });
   }
 }
 
