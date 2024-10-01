77a9af86bd804c2f39b693557839c58522439ed1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-10-01 17:36:06 +0100

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
+
 class AuthController {
   static async getConnect(request, response) {
     const authData = request.header('Authorization');
