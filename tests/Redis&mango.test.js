991e72d13474c283ba8cf2904d379c51fe038135
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-10-02 14:23:34 +0100

    Update Redis&mango.test.js

--- tests/Redis&mango.test.js
+++ tests/Redis&mango.test.js
@@ -7,38 +7,21 @@ import redisClient from '../utils/redis';
 use(chaiHttp);
 should();
 
-// redisClient
-
-describe('testing the clients for MongoDB and Redis', () => {
+describe('testing the clients', () => {
   describe('redis Client', () => {
-    before(async () => {
-      await redisClient.client.flushall('ASYNC');
-    });
+    before(async () => await redisClient.client.flushall('ASYNC'));
+    after(async () => await redisClient.client.flushall('ASYNC'));
 
-    after(async () => {
-      await redisClient.client.flushall('ASYNC');
-    });
-
-    it('shows that connection is alive', async () => {
-      expect(redisClient.isAlive()).to.equal(true);
-    });
-
-    it('returns key as null because it does not exist', async () => {
-      expect(await redisClient.get('myKey')).to.equal(null);
-    });
-
-    it('set key can be called without issue', async () => {
-      expect(await redisClient.set('myKey', 12, 1)).to.equal(undefined);
-    });
-
-    it('returns key with null because it expired', async () => {
+    it('shows connection is alive', async () => expect(redisClient.isAlive()).to.equal(true));
+    it('returns key as null', async () => expect(await redisClient.get('myKey')).to.equal(null));
+    it('set key without issue', async () => expect(await redisClient.set('myKey', 12, 1)).to.equal(undefined));
+    it('returns key with null after expiry', async () => {
       const sleep = promisify(setTimeout);
       await sleep(1100);
       expect(await redisClient.get('myKey')).to.equal(null);
     });
   });
 
-  // dbClient
   describe('db Client', () => {
     before(async () => {
       await dbClient.usersCollection.deleteMany({});
@@ -49,27 +32,17 @@ describe('testing the clients for MongoDB and Redis', () => {
       await dbClient.filesCollection.deleteMany({});
     });
 
-    it('shows that connection is alive', () => {
-      expect(dbClient.isAlive()).to.equal(true);
-    });
-
-    it('shows that connection is alive', () => {
-      expect(dbClient.isAlive()).to.equal(true);
-    });
-
+    it('shows connection is alive', () => expect(dbClient.isAlive()).to.equal(true));
     it('shows number of user documents', async () => {
       await dbClient.usersCollection.deleteMany({});
       expect(await dbClient.nbUsers()).to.equal(0);
-
       await dbClient.usersCollection.insertOne({ name: 'Larry' });
       await dbClient.usersCollection.insertOne({ name: 'Karla' });
       expect(await dbClient.nbUsers()).to.equal(2);
     });
-
     it('shows number of file documents', async () => {
       await dbClient.filesCollection.deleteMany({});
       expect(await dbClient.nbFiles()).to.equal(0);
-
       await dbClient.filesCollection.insertOne({ name: 'FileOne' });
       await dbClient.filesCollection.insertOne({ name: 'FileTwo' });
       expect(await dbClient.nbUsers()).to.equal(2);
